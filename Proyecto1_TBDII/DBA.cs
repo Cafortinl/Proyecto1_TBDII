bbc415d1debb290ff7d59e0c2f76b926fc273ffe
Carlos Fortín <cafortinl@hotmail.com>
2021-05-19 19:01:32 -0600

    First commit

---
+++ Proyecto1_TBDII/DBA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Redis;
+
+namespace Proyecto1_TBDII
+{
+    class DBA{ 
+        private IDatabase conn;
+        public DBA() { }
+        public DBA(string connString)
+        {
+            ConnectionMultiplexer muxer = ConnectionMultiplexer.Connect(connString);
+            conn = muxer.GetDatabase();
+        }
+
+        public IDatabase getConn()
+        {
+            return conn;
+        }
+
+        public void setKey(string k, string v)
+        {
+            conn.StringSet(k,v);
+        }
+
+        public string getValue(string k)
+        {
+            return conn.StringGet(k);
+        }
+
+    }
+}
---
+++ Proyecto1_TBDII/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using StackExchange.Redis;
+
+namespace Proyecto1_TBDII
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        DBA dba = new DBA("127.0.0.1:6379,password=1");
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            int id = -1;
+            string user, pass;
+            user = tbUsuario.Text;
+            pass = pbPassword.Password;
+            IDatabase conn = dba.getConn();
+            var hash = conn.HashGetAll("Login:"+user);
+            string data="";
+            for(int i = 0;i < 3;i++)
+            {
+                if (i == 3)
+                    id = Convert.ToInt32(hash[i].Value);
+            }
+            MessageBox.Show(Convert.ToString(id));
+        }
+    }
+}
